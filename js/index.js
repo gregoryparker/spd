44f0140cc6697f9506de85a9fad413adcc7a486d
Varun <varunshivakumar@outlook.com>
2016-01-23 20:49:13 -0500

    First Commit for SPD Website

---
+++ js/index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by vshivakumar on 12/9/15.
+ */
+
+
+$(document).ready(function(){
+    $("#slideshow > div:gt(0)").hide();
+
+    setInterval(function() {
+        $('#slideshow > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('#slideshow');
+    },  5000);
+
+
+
+
+
+});
